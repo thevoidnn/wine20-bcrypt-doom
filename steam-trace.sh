df4f229e4126fb5f20074b3793a22a2682b78c7e
thevoidnn <thevoidnn@gmail.com>
2016-12-16 20:40:58 +0300

    scripts

---
+++ steam-trace.sh
@@ -0,0 +1,3 @@
+cd wine-2.0-rc1-64-build
+
+WINEPREFIX="/games/wine/steam/wineprefix" WINEDEBUG="-all,+winhttp" ./wine "C:\\\\Steam\\\\Steam.exe" "-no-cef-sandbox"
---
+++ steam.sh
@@ -0,0 +1,3 @@
+cd wine-2.0-rc1-64-build
+
+WINEPREFIX="/games/wine/steam/wineprefix" ./wine "C:\\\\Steam\\\\Steam.exe" "-no-cef-sandbox"
